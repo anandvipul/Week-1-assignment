eace0aefedee9f51acf4d244f655565531a8fd8e
anand <anandvipul01@gmail.com>
2023-06-27 15:25:18 +0530

    todo

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -12,7 +12,39 @@
 */
 
 class Todo {
+  constructor() {
+    this.todoList = [];
+  }
 
+  add = (todo) => {
+    this.todoList.push(todo);
+  };
+
+  remove = (index) => {
+    this.todoList.splice(index, 1);
+  };
+
+  update = (index, todo) => {
+    if (index > this.todoList.length - 1) {
+      return;
+    }
+    this.todoList.splice(index, 1, todo);
+  };
+
+  getAll = () => {
+    return this.todoList;
+  };
+
+  get = (index) => {
+    if (index > this.todoList.length - 1) {
+      return null;
+    }
+    return this.todoList[index];
+  };
+
+  clear = () => {
+    this.todoList = [];
+  };
 }
 
 module.exports = Todo;
