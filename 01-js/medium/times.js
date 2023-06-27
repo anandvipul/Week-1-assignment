4de5d9a412e2f0efaa7cecc15a71e3c2e401c58a
anand <anandvipul01@gmail.com>
2023-06-27 15:36:12 +0530

    times

--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -8,5 +8,18 @@ Hint - use Date class exposed in JS
 */
 
 function calculateTime(n) {
-    return 0.01;
+  let startTime = performance.now();
+  function sumOfNums(n) {
+    let sum = 0;
+    for (let i = 1; i <= n; i++) {
+      sum += i;
+    }
+  }
+  let endTime = performance.now();
+  let runTime = endTime - startTime;
+
+  return runTime;
 }
+
+console.log(calculateTime(200));
+console.log(calculateTime(300));
