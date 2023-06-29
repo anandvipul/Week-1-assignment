04a5f6d9585aee19885b4141fb8405110b946f58
anand <anandvipul01@gmail.com>
2023-06-29 14:51:53 +0530

    promise all

--- 02-async-js/hard (promises)/3-promise-all.js
+++ 02-async-js/hard (promises)/3-promise-all.js
@@ -4,19 +4,36 @@
  * Print how long it took for all 3 promises to resolve.
  */
 
-
 function waitOneSecond() {
-
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('One Second');
+    }, 1000);
+  });
 }
 
 function waitTwoSecond() {
-
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('Two Seconds');
+    }, 2000);
+  });
 }
 
 function waitThreeSecond() {
-
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('Three Seconds');
+    }, 3000);
+  });
 }
 
-function calculateTime() {
-
+async function calculateTime() {
+  console.log('Started the sequential Code');
+  await waitOneSecond().then(console.log);
+  await waitTwoSecond().then(console.log);
+  await waitThreeSecond().then(console.log);
+  console.log('Finished the code execution');
 }
+
+calculateTime();
